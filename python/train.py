322dad3592a334d71b9cc90c543da677bee80235
PirashanthR <rpirashanth@gmail.com>
2021-05-19 00:41:14 +0200

    volatile=True -> torch no grad

--- python/train.py
+++ python/train.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 import models, loader, optim
 import numpy as np
 from utils import *
+import torch
 
 parser = argparse.ArgumentParser(description='PyTorch Entropy-SGD')
 ap = parser.add_argument
@@ -96,56 +97,31 @@ def train(e):
     print('Train: [%2d] %2.4f %2.2f%% [%.2fs]'% (e, fs.avg, top1.avg, timer()-ts))
     print()
 
-def set_dropout(cache = None, p=0):
-    if cache is None:
-        cache = []
-        for l in model.modules():
-            if 'Dropout' in str(type(l)):
-                cache.append(l.p)
-                l.p = p
-        return cache
-    else:
-        for l in model.modules():
-            if 'Dropout' in str(type(l)):
-                assert len(cache) > 0, 'cache is empty'
-                l.p = cache.pop(0)
-
-def dry_feed():
-    cache = set_dropout()
-    maxb = int(math.ceil(train_loader.n/opt['b']))
-    for bi in range(maxb):
-        x,y = next(train_loader)
-        if opt['cuda']:
-            x,y = x.cuda(), y.cuda()
-        x,y =   Variable(x, volatile=True), \
-                Variable(y.squeeze(), volatile=True)
-        yh = model(x)
-    set_dropout(cache)
-
 def val(e, data_loader):
-    dry_feed()
     model.eval()
 
     maxb = int(math.ceil(data_loader.n/opt['b']))
-
+    print("maxb is %d"%maxb)
+    
     fs, top1 = AverageMeter(), AverageMeter()
-    for bi in range(maxb):
-        x,y = next(data_loader)
-        bsz = x.size(0)
+    with torch.no_grad():
+        for bi in range(maxb):
+            x,y = next(data_loader)
+            bsz = x.size(0)
 
-        if opt['cuda']:
-            x,y = x.cuda(), y.cuda()
+            if opt['cuda']:
+                x,y = x.cuda(), y.cuda()
 
-        x,y =   Variable(x, volatile=True), \
-                Variable(y.squeeze(), volatile=True)
-        yh = model(x)
+            x,y =   Variable(x), \
+                    Variable(y.squeeze())
+            yh = model(x)
 
-        f = criterion.forward(yh, y).data.item()
-        prec1, = accuracy(yh.data, y.data, topk=(1,))
-        err = 100-prec1.item()
+            f = criterion.forward(yh, y).data.item()
+            prec1, = accuracy(yh.data, y.data, topk=(1,))
+            err = 100-prec1.item()
 
-        fs.update(f, bsz)
-        top1.update(err, bsz)
+            fs.update(f, bsz)
+            top1.update(err, bsz)
 
     print('Test: [%2d] %2.4f %2.4f%%\n'%(e, fs.avg, top1.avg))
     print()
